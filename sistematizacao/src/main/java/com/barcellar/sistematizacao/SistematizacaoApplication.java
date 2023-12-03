3285ab25f60caf4b5d2233a98e8c7237a314f4b5
Fabio Barcellar <barcellar80@gmail.com>
2023-12-03 01:40:52 -0300

    Inicial

---
+++ sistematizacao/src/main/java/com/barcellar/sistematizacao/SistematizacaoApplication.java
@@ -0,0 +1,13 @@
+package com.barcellar.sistematizacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistematizacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistematizacaoApplication.class, args);
+	}
+
+}
---
+++ sistematizacao/src/test/java/com/barcellar/sistematizacao/SistematizacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barcellar.sistematizacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistematizacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
